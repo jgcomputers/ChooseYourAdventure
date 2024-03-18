71869a83f6fe1e302a73450ad3b409ab828de26e
Gibbs <jgcomputers132@gmail.com>
2024-03-18 13:14:20 -0400

    Welcome Message in Function - Stable

--- ChooseYourAdventure1.0.py
+++ ChooseYourAdventure1.0.py
@@ -1,3 +1,14 @@
 # Programmer: Jordan Gibbs
 # Date: 3.15.2024
 # Program: Choose Your Adventure
+
+# import libraries here
+import time
+
+def main():
+
+    # Welcome message
+    print("Welcome to the Adventure Game!")
+
+if __name__ == "__main__":
+    main()
