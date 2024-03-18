eee4eb6baa786c33a3cfbb71c3b0d9c858e327b7
Gibbs <jgcomputers132@gmail.com>
2024-03-18 13:22:57 -0400

    Give player attributes

--- ChooseYourAdventure1.0.py
+++ ChooseYourAdventure1.0.py
@@ -2,13 +2,20 @@
 # Date: 3.15.2024
 # Program: Choose Your Adventure
 
-# import libraries here
+# import libraries here:
 import time
 
 def main():
+    # Initialize player attributes
+    player = {
+        "name": "Guy",
+        "strength": 6,
+        "max_strength": 10,
+        "actions": {
+            "1": "go get ice cream",
+            "2": "go to the weight room",
+        }
+    }
 
     # Welcome message
     print("Welcome to the Adventure Game!")
-
-if __name__ == "__main__":
-    main()
